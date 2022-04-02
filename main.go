b1c33fcf521442cf265522f0e3256ad96cef6c08
libra82 <david.tao.zh@gmail.com>
2022-04-02 12:24:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	fmt.Print(wxAuth.GetRedirectUrl("sate")) //获取第三方登录地址
 
-	wxRes, err := wxAuth.GetToken("code")
+	wxRes, err := wxAuth.GetAppAccessToken("code")
 
 	userInfo, _ := wxAuth.GetUserInfo(wxRes.AccessToken, wxRes.OpenId)
 
